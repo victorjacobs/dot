48ebb820a72a8c4cfcfe8000089558693639a545
Victor Jacobs <victor@vjcbs.dev>
2020-12-24 11:53:56 +0100

    Create directories for backed up dot files during setup

--- setup.sh
+++ setup.sh
@@ -8,10 +8,15 @@ function config {
 mkdir -p .config-backup
 
 if config checkout; then
-    echo "Checked out config.";
+    echo "Checked out config."
 else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | grep -E "\s+\." | awk '{$1=$1;print}' | xargs -I{} mv {} .config-backup/{}
+    echo "Backing up pre-existing dot files..."
+    
+    for file in $(config checkout 2>&1 | grep -E "\s+\." | awk '{$1=$1;print}'); do
+        echo "Moving $file"
+        mkdir -p .config-backup/$(dirname $file)
+        mv $file .config-backup/$file
+    done
 fi;
 
 config checkout
