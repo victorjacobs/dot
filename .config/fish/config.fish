9dff405a7d7ffdd573077901b7eaf7f70b22e7c8
Victor Jacobs <victor@vjcbs.dev>
2022-03-27 14:43:45 +0200

    Install Fisher plugins on Fisher install

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,14 +2,15 @@ if not functions -q fisher
     set FISHER_VERSION "4.3.1"
     set FISHER_CHECKSUM "34c6f4cb4847d27bd7f081ebeafc10585eea31d3a1b1f7bf630108fc658c9529"
 
-    set FISHER_URL "https://raw.githubusercontent.com/jorgebucaran/fisher/$FISHER_VERSION/fisher.fish"
+    set FISHER_URL "https://raw.githubusercontent.com/jorgebucaran/fisher/$FISHER_VERSION/functions/fisher.fish"
     set REMOTE_CHECKSUM (curl -Lks $FISHER_URL | shasum -a 256 | awk '{print $1}')
 
     if [ $FISHER_CHECKSUM != $REMOTE_CHECKSUM ]
         echo "Fisher install failed, checksums don't match"
     else
         echo "Downloading fisher..."
-        curl -sL $FISHER_URL | source && fisher install jorgebucaran/fisher
+        set --local plugins (read --null <~/.config/fish/fishfile)
+        curl -sL $FISHER_URL | source && fisher install jorgebucaran/fisher $plugins
     end
 end
 
